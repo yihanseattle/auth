4ebab0126e354f472b367cce112373e823a57937
yihanseattle <yihanseattle@gmail.com>
2016-12-12 15:34:59 -0800

    L58.

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header} from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
   componentWillMount() {
     firebase.initializeApp({
-    apiKey: 'AIzaSyCnJSTTFkCRFzJrwAuoxcicpxBCiUEZOcc',
-    authDomain: 'auth-b0612.firebaseapp.com',
-    databaseURL: 'https://auth-b0612.firebaseio.com',
-    storageBucket: 'auth-b0612.appspot.com',
-    messagingSenderId: '17138977706'
+      apiKey: 'AIzaSyCnJSTTFkCRFzJrwAuoxcicpxBCiUEZOcc',
+      authDomain: 'auth-b0612.firebaseapp.com',
+      databaseURL: 'https://auth-b0612.firebaseio.com',
+      storageBucket: 'auth-b0612.appspot.com',
+      messagingSenderId: '17138977706'
     });
   }
 
@@ -19,7 +20,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
