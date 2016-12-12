0eeb78416ce53be8c23462cf6b8b282ff754b235
yihanseattle <yihanseattle@gmail.com>
2016-12-12 15:43:11 -0800

    L59.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
+        <CardSection >
+          <TextInput style={{ height: 40, width: 100 }}/>
+        </CardSection>
+
         <CardSection />
-        <CardSection />
+
         <CardSection >
           <Button>
             Log in
