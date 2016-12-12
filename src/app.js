0494d51738a4a03f66fbca26aa35326f4cba06e3
yihanseattle <yihanseattle@gmail.com>
2016-12-12 15:26:50 -0800

    L57.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header} from './components/common';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyCnJSTTFkCRFzJrwAuoxcicpxBCiUEZOcc',
+    authDomain: 'auth-b0612.firebaseapp.com',
+    databaseURL: 'https://auth-b0612.firebaseio.com',
+    storageBucket: 'auth-b0612.appspot.com',
+    messagingSenderId: '17138977706'
+    });
+  }
+
   render() {
     return (
       <View>
