15b0ea5b8e699f2e711b4cb9b769d8d38e485986
yihanseattle <yihanseattle@gmail.com>
2016-12-15 17:16:14 -0800

    L72.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -14,13 +14,33 @@ class LoginForm extends Component {
     this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            this.setState({ error: 'Authentication Failed.' });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
       });
+  }
 
+  onLoginFail() {
+    // set the error messages
+    // stop the spinner
+    this.setState({
+      error: 'Authentication Failed.',
+      loading: false
+    });
+  }
+
+  onLoginSuccess() {
+    // clear error messages
+    // not loading anymore
+    // clear out the form (email, password)
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
   }
 
   renderButton() {
