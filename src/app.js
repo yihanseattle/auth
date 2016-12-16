392e2e955b935e610ad02c9f2469755c7b119080
yihanseattle <yihanseattle@gmail.com>
2016-12-16 14:25:04 -0800

    L74.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header} from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -26,11 +26,31 @@ class App extends Component {
     });
   }
 
+  renderContent() {
+
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button >
+              "Log Out"
+            </Button>
+          </CardSection>
+        );
+      case false:
+        // not loggedIn
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
